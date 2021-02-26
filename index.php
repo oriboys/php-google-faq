45112b22b2ae21a66b157b5910533cfa8d5d407a
andrea <corro.andrea.1995@gmail.com>
2021-02-26 13:33:14 +0100

    init

---
+++ index.php
@@ -0,0 +1,24 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="css/style.css">
+  <title>Document</title>
+</head>
+<body>
+
+<div class="container">
+  <footer>
+    <div class="footer-top"></div>
+    <div class="footer-bottom"></div>
+  </footer>
+  <div class="jumbo">
+
+  </div>
+</div>
+</body>
+</html>
